9c798b229fbe55541154efa3f3a1acd25a14baa8
arribbar <bartychou@gmail.com>
2013-10-03 03:06:14 +0200

    ajout date, mail, auteur ds extract_grostitres.pl, error, il fait une iteration en trop, to fix

--- extract_grostitres.pl
+++ extract_grostitres.pl
@@ -13,32 +13,52 @@ foreach $line (<FILE>) {
 	
 	#sort tous les gros titres seulement avec auteur et email
 	#mailto:(.*?)" class="S14">(.*?)<\/a>(.*?)
-    $line =~ /<GROSTITRE>(.*?)"><img src="(.*?)"(.*?)class="S301">(.*?)<\/span>(.*?)class="S63">(.*?)<\/a>(.*?)class="S48">(.*?)<\/a><span class="S48">\((.*?)\)(.*?)/;
+    $line =~ /<GROSTITRE>(.*?)"><img src="(.*?)"(.*?)class="S301">(.*?)<\/span>(.*?)class="S63">(.*?)<\/a>(.*?)class="S48">(.*?)<\/a>(.*?)<\/table>(.*?)/;
 	$status = $&;
-	$url_article = "/".$1;
+	$url_article = $1;
 	$theme_article = $4;
 	$titre_article = $6;
-	$date_article = $9;
+#	$date_article = "";
 	$url_image = $2;
 	$resume_article = $8;
-	$mailto = $11;
-	$auteur = $12;
+	$result_date = $result_author = $9;
+
+	$result_date =~ /<span class="S48">\((.*?)\)(.*?)/;
+	if ($url_article ne $1){
+		$date_article = $1;
+		}
+
+	$result_author =~ /(.*?)mailto:(.*?)" class="S14">(.*?)<\/a>(.*?)/;	
+		if ($url_image ne $2){
+			$mailto = $2;	
+			$author = $3;
+			}
+	
 
 		
 	if ($prev ne $status) {
+	
 		print "	
 		<GROSTITRE>
-			<urlArticle>".$url_article."</urlArticle>
+		    <urlArticle>"."/".$url_article."</urlArticle>
 		    <themeArticle>".$theme_article."</themeArticle>
 		    <titreArticle>".$titre_article."</titreArticle>
 		    <dateArticle>".$date_article."</dateArticle>
 		    <urlImage>".$url_image."</urlImage>
 		    <resumeArticle>".$resume_article."</resumeArticle>
 		    <mailto>".$mailto."</mailto>
-		    <auteur>".$auteur."</auteur>
+		    <auteur>".$author."</auteur>
 		</GROSTITRE>";
+		
+		$date_article = "";
+		$mailto = "";
+		$author = "";
+		$url_article = "";
+		$url_image = "";
+
 	}
 	$prev = $status;
 }
 
-print "\n	"
+print "\n	";
+
