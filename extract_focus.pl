648f5fe2e48fa93e70a9672258ffde49690598d9
arribbar <bartychou@gmail.com>
2013-10-03 19:26:55 +0200

    affiche date pour la une et le focus

--- extract_focus.pl
+++ extract_focus.pl
@@ -5,26 +5,32 @@ $fichier = $ARGV[0];
 
 open(FILE,$fichier) or die("Could not open the file.");
 
+$date_article = "";
 foreach $line (<FILE>) {
+	if ($date_article eq ""){
+		$line =~ /(.*?)<date>(.*?)<\/date>(.*?)/;
+		$date_article = $2;
+	}
 
-#[mailto:(.*?)" class="S14">(.*?)<\/a>(.*?)]
+	
 	 
-	$line =~ /<FOCUS>(.*?)<a href="(.*?)"><img src="(.*?)"(.*?)class="S401">(.*?)<\/a>(.*?)class="S48">(.*?)<\/a>(.*?)<\/FOCUS>/;
-	#mailto:(.*?)" class="S14">(.*?)<\/a>(.*?)
+		$line =~ /<FOCUS>(.*?)<a href="(.*?)"><img src="(.*?)"(.*?)class="S401">(.*?)<\/a>(.*?)class="S48">(.*?)<\/a>(.*?)<\/FOCUS>/;
+		#mailto:(.*?)" class="S14">(.*?)<\/a>(.*?)
 	
 	
-	$url_article = $2;
-	$titre_article = $5;
-	$date_article = "";
-	$url_image = $3;
-	$resume_article = $7;
-	$result = $8;
+		$url_article = $2;
+		$titre_article = $5;
+		$url_image = $3;
+		$resume_article = $7;
+		$result = $8;
 	
+
 
 }
-	$result =~ / (.*?)mailto:(.*?)" class="S14">(.*?)<\/a>(.*?)/;
-	$mailto = $2;
-	$auteur = $3;
+
+$result =~ / (.*?)mailto:(.*?)" class="S14">(.*?)<\/a>(.*?)/;
+$mailto = $2;
+$auteur = $3;
 
 print "	
 		<urlArticle>".$url_article."</urlArticle>
@@ -35,4 +41,4 @@ print "
 		<mailto>".$mailto."</mailto>
 		<auteur>".$auteur."</auteur>
 	";
-	
+		print $date_article;
--- extract_une.pl
+++ extract_une.pl
@@ -7,10 +7,14 @@ open(FILE,$fichier) or die("Could not open the file.");
 
 foreach $line (<FILE>) {
 
+
+	if ($date_article eq ""){
+		$line =~ /(.*?)<date>(.*?)<\/date>(.*?)/;
+		$date_article = $2;
+	}
+ 
 	### GET UNE - IBL_ID=27914
-	#<a href="mailto:agautier@tf1.fr" class="S14">AFP</a>
-	#[mailto:(.*?)" class="S14">(.*?)<\/a>(.*?)]
-	### le <\/a> avant le mailto restraint tout. le mailto et auteur sont dans $11
+
 	$line =~ /<UNE>(.*?)href="(.*?)" class="S431"(.*?)>(.*?)<\/a>(.*?)<img src="(.*?)"(.*?)class="S48">(.*?)<img(.*?)> (.*?)<\/a>(.*?)<\/UNE>/;
 	$titre_article = $4;
 	$url_article = $2;
@@ -26,7 +30,7 @@ foreach $line (<FILE>) {
 print "		
 		<urlArticle>".$url_article."</urlArticle>
 	 	<titreArticle>".$titre_article."</titreArticle>
-		<dateArticle></dateArticle>
+		<dateArticle>.$date_article.</dateArticle>
 		<urlImage>".$url_image."</urlImage>
 		<resumeArticle>".$resume_article."</resumeArticle>
 		<mailto>".$mailto."</mailto>
